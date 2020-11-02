8f77e6c35cd4849fa9f910fb9f80aeb885e69554
G-shino <shinogu2929@gmail.com>
2020-11-02 22:38:56 +0900

    add image

--- tutorialForBeginners/lib/main.dart
+++ tutorialForBeginners/lib/main.dart
@@ -1,29 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar:AppBar(
-      title: Text("my first app"),
-      centerTitle: true,
-      backgroundColor: Colors.red[600],
-    ),
-    body: Center(
-      child: Text(
-          "hello ninjas!",
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.grey[600],
-            fontFamily: "IndieFlower",
-      )),
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Text("click"),
-      onPressed: () => {},
-      backgroundColor: Colors.red[600],
-    ),
-  ),
+  home: Home(),
 ));
 
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:AppBar(
+        title: Text("tutorial for beginners"),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Image.asset("assets/dog2.jpg"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text("click"),
+        onPressed: () => {},
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
 
